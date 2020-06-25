b9dcc0db0a6964b43c54fcd922db8c40365f31ac
Flyfish Wang <ffgh2010@live.cn>
2020-06-25 21:35:04 +0800

    'Release' Alpha 1
    Alpha 1

--- config.sh
+++ config.sh
@@ -1,35 +1,15 @@
 AUTOMOUNT=false
-
-# Set to true if you need to load system.prop
 PROPFILE=false
-
-# Set to true if you need post-fs-data script
 POSTFSDATA=false
-
-# Set to true if you need late_start service script
 LATESTARTSERVICE=false
-
-##########################################################################################
-# Installation Message
-##########################################################################################
-
-# Set what you want to show when installing your mod
 
 print_modname() {
   ui_print "*******************************"
   ui_print "*               FUCoolapk       *"
-  ui_print "*       A L P H A: MAY NOT WORKING    *"
+  ui_print "*       Alpha 1: MAY NOT WORKING    *"
   ui_print "*******************************"
 }
 
-##########################################################################################
-# Replace list
-##########################################################################################
-
-# List all directories you want to directly replace in the system
-# Check the documentations for more info about how Magic Mount works, and why you need this
-
-# This is an example
 REPLACE="
 /system/app/Youtube
 /system/priv-app/SystemUI
@@ -37,30 +17,11 @@ REPLACE="
 /system/framework
 "
 
-# Construct your own list here, it will override the example above
-# !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
 REPLACE="
 "
 
-##########################################################################################
-# Permissions
-##########################################################################################
 
 set_permissions() {
-  # Only some special files require specific permissions
-  # The default permissions should be good enough for most cases
-
-  # Here are some examples for the set_perm functions:
-
-  # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
-  # set_perm_recursive  $MODPATH/system/lib       0       0       0755            0644
-
-  # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
-  # set_perm  $MODPATH/system/bin/app_process32   0       2000    0755         u:object_r:zygote_exec:s0
-  # set_perm  $MODPATH/system/bin/dex2oat         0       2000    0755         u:object_r:dex2oat_exec:s0
-  # set_perm  $MODPATH/system/lib/libart.so       0       0       0644
-
-  # The following is default permissions, DO NOT remove
   set_perm_recursive  $MODPATH  0  0  0755  0644
 }
 
